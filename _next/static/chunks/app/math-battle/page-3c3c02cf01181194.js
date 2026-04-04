1e9ab489ee87aca3f6f5f9933f67dc9e833766bb
inti25 <inti25@upfi.network>
2026-04-04 07:54:55 +0700

    update

--- _next/static/chunks/app/math-battle/page-3c3c02cf01181194.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[50],{8381:(e,s,_)=>{Promise.resolve().then(_.bind(_,9936))}},e=>{var s=s=>e(e.s=s);e.O(0,[905,737,977,181,857,555,441,684,358],()=>s(8381)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/math-battle/page-5ca7535b6ffa1f24.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[50],{8381:(e,s,_)=>{Promise.resolve().then(_.bind(_,9936))}},e=>{var s=s=>e(e.s=s);e.O(0,[46,411,181,347,555,441,684,358],()=>s(8381)),_N_E=e.O()}]);
